35d1a6514455caf1d61f0c526e907b5f49011659
Patryk Gramatowski <patrick.gramatowski@gmail.com>
2021-05-29 22:12:26 +0200

    Add test for hall model

--- spec/models/hall_spec.rb
+++ spec/models/hall_spec.rb
@@ -1,5 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe Hall, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'validations' do
+    subject { build(:hall) }
+    context 'has hall attribute' do
+      it 'must be present' do
+        expect(subject).to be_valid
+        subject.hall = nil
+        expect(subject).to_not be_valid
+      end
+    end
+
+    context 'has seats attribute' do
+      it 'must be present' do
+        expect(subject).to be_valid
+        subject.seats = nil
+        expect(subject).to_not be_valid
+      end
+    end
+  end
 end
