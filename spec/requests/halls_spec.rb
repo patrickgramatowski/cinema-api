4a33b559b33db180e68e69fd9441af50f5121524
Patryk Gramatowski <patrick.gramatowski@gmail.com>
2021-05-30 13:06:41 +0200

    Add tests for halls

--- spec/requests/halls_spec.rb
+++ spec/requests/halls_spec.rb
@@ -30,17 +30,35 @@ RSpec.describe "Halls requests" do
     end
   end
 
-  describe "POST /halls" do
+  describe "POST /api/halls" do
     it "works and return status 201" do
       post("/api/halls", params: { hall: {  hall: "Hall_2", seats: 150 } } )
       expect(response.status).to eq(201)
     end
   end
 
-  describe "POST /halls" do
+  describe "POST /api/halls" do
     it "does not work and return status 422" do
       post("/api/halls", params: { hall: {  hall: "Hall_2" } } )
       expect(response.status).to eq(422)
+    end
+  end
+
+  describe 'PUT /api/halls/:id' do
+    let!(:hall) { create(:hall) }
+
+    it 'works and returns status 200' do
+      put("/api/halls/#{hall.id}", params: { hall: { id: hall.id, name: "Hall_updated" } })
+      expect(response.status).to eq(200)
+    end
+  end
+
+  describe 'DELETE /api/halls/:id' do
+    let!(:hall) { create(:hall) }
+
+    it 'works and return status 204' do
+      delete("/api/halls/#{hall.id}")
+      expect(response.status).to eq(204)
     end
   end
 end
