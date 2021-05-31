fd3749335fa723603d52925389ccaa1fc1a7f51f
Patryk Gramatowski <patrick.gramatowski@gmail.com>
2021-05-31 17:06:41 +0200

    Create concpets for seance model

---
+++ app/concepts/seances/use_cases/delete.rb
@@ -0,0 +1,11 @@
+module Seances
+  module UseCases
+    class Delete < ::UseCases::BaseDelete
+      attr_reader :repository
+
+      def initialize(repository: Seances::Repository.new)
+        super(repository: repository)
+      end
+    end
+  end
+end
