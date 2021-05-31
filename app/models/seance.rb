ec74ef23bce5928dd75f0edd79077d0a025a1b28
Patryk Gramatowski <patrick.gramatowski@gmail.com>
2021-05-31 16:41:03 +0200

    Add validations for seance model

--- app/models/seance.rb
+++ app/models/seance.rb
@@ -1,4 +1,9 @@
 class Seance < ApplicationRecord
   belongs_to :movie
   belongs_to :hall
+
+  validates :date, presence: true
+  validates :time, presence: true
+  validates :movie_id, presence: true
+  validates :hall_id, presence: true
 end
