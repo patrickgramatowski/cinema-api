05156d7004a84f99c978ff8f500a58081398c99f
Patryk Gramatowski <patrick.gramatowski@gmail.com>
2021-06-02 13:15:02 +0200

    Add validations for ticket_desk and ticket models

---
+++ app/models/ticket.rb
@@ -0,0 +1,8 @@
+class Ticket < ApplicationRecord
+  belongs_to :reservation
+
+  validates :price, presence: true
+  validates :seat, presence: true
+  validates :ticket_type, presence: true
+  validates :reservation_id, presence: true
+end
