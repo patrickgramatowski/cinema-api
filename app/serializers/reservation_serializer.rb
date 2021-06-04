77a26b37bf86a983e89ebce1765031cdc6aff17e
Patryk Gramatowski <patrick.gramatowski@gmail.com>
2021-06-04 12:18:49 +0200

    Update reservation serializer

--- app/serializers/reservation_serializer.rb
+++ app/serializers/reservation_serializer.rb
@@ -1,7 +1,8 @@
 class ReservationSerializer
   include JSONAPI::Serializer
-  attributes :seance, :ticket_desk, :status
+  attributes :seance, :ticket_desk, :status, :tickets
 
+  has_many :tickets
   has_one :ticket_desk
   has_one :seance
 end
