c716dfcba2a69ad24443c81734845f4d8b84864a
Patryk Gramatowski <patrick.gramatowski@gmail.com>
2021-06-04 12:22:56 +0200

    Add available seats use case for seances

---
+++ app/concepts/seances/use_cases/available_seats.rb
@@ -0,0 +1,19 @@
+module Seances
+  module UseCases
+    class AvailableSeats
+      attr_reader :repository
+
+      def initialize(repository: Seances::Repository.new)
+        @repository = repository
+      end
+
+      def call(id:)
+        seance = repository.find(id)
+        taken_seats = seance.reservations.flat_map(&:tickets).map(&:seat)
+        all_seats = Halls::UseCases::GenerateSeats.new.call(hall_id: seance.hall_id)
+        available_seats = all_seats - taken_seats
+        { movie: seance.movie, hall: seance.hall, seats: available_seats, taken_seats: taken_seats }.to_json
+      end
+    end
+  end
+end
