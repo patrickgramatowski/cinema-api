55614bf730d50a9b0e616356ae1b0e59024aad08
Patryk Gramatowski <patrick.gramatowski@gmail.com>
2021-06-04 12:19:08 +0200

    Update reservation model

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -1,6 +1,7 @@
 class Reservation < ApplicationRecord
   belongs_to :seance
   belongs_to :ticket_desk
+  has_many :tickets
 
   validates :seance_id, presence: true
   validates :ticket_desk_id, presence: true
