6acb74370dbfd064fcac3427a7ec4c7bbebe59f5
Patryk Gramatowski <patrick.gramatowski@gmail.com>
2021-06-08 10:20:13 +0200

    Fix ticket tests

--- spec/requests/tickets_spec.rb
+++ spec/requests/tickets_spec.rb
@@ -3,22 +3,24 @@ require "rspec_api_documentation/dsl"
 
 RSpec.describe "Tickets requests" do
   let(:ticket_desk) { create(:ticket_desk).id }
+  let(:reservation) { create(:reservation).id }
 
   before do
     ticket_desk
+    reservation
   end
 
-  describe "GET /api/ticket_desks/:id/tickets" do
+  describe "GET /api/ticket_desks/:id/reservations/:id/tickets" do
     let!(:ticket) { build(:ticket) }
 
     it "works and return status 200" do
-      get("/api/ticket_desks/#{ticket_desk}/tickets")
+      get("/api/ticket_desks/#{ticket_desk}/reservations/#{reservation}/tickets")
       expect(response.status).to eq(200)
     end
   end
 
   resource "tickets" do
-    get("/api/tickets") do
+    get("/api/ticket_desks/1/reservations/1/tickets") do
       example "Gets list of tickets" do
         do_request
         
@@ -27,45 +29,45 @@ RSpec.describe "Tickets requests" do
     end
   end
 
-  describe "GET /api/ticket_desks/:id/tickets/:id" do
+  describe "GET /api/ticket_desks/:id/reservations/:id/tickets/:id" do
     let!(:ticket) { build(:ticket) }
 
     it "works and return status 200" do
-      get("/api/ticket_desks/#{ticket_desk}/tickets/#{ticket.id}")
+      get("/api/ticket_desks/#{ticket_desk}/reservations/#{reservation}/tickets/#{ticket.id}")
       expect(response.status).to eq(200)
     end
   end
 
-  describe "POST /api/ticket_desks/:id/tickets" do
+  describe "POST /api/ticket_desks/:id/reservations/:id/tickets" do
     let(:reservation) { create(:reservation) }
 
     it "works and return status 201" do
-      post("/api/ticket_desks/#{ticket_desk}/tickets", params: { ticket: { price: 22, ticket_type: "Normal", seat: "1A", reservation_id: reservation.id  } } )
+      post("/api/ticket_desks/#{ticket_desk}/reservations/#{reservation.id}/tickets", params: { ticket: { price: 22, ticket_type: "Normal", seat: "1A", reservation_id: reservation.id  } } )
       expect(response.status).to eq(201)
     end
   end
 
-  describe "POST /api/ticket_desks/:id/tickets" do
+  describe "POST /api/ticket_desks/:id/reservations/:id/tickets" do
     it "does not work and return status 422" do
-      post("/api/ticket_desks/#{ticket_desk}/tickets", params: { ticket: { price: 11 } } )
+      post("/api/ticket_desks/#{ticket_desk}/reservations/#{reservation}/tickets", params: { ticket: { price: 11 } } )
       expect(response.status).to eq(422)
     end
   end
 
-  describe 'PUT /api/ticket_desks/:id/tickets/:id' do
+  describe 'PUT /api/ticket_desks/:id/reservations/:id/tickets/:id' do
     let!(:ticket) { create(:ticket) }
 
     it 'works and returns status 200' do
-      put("/api/ticket_desks/#{ticket_desk}/tickets/#{ticket.id}", params: { ticket: { price: 111 } })
+      put("/api/ticket_desks/#{ticket_desk}/reservations/#{reservation}/tickets/#{ticket.id}", params: { ticket: { price: 111 } })
       expect(response.status).to eq(200)
     end
   end
 
-  describe 'DELETE /api/ticket_desks/:id/tickets/:id' do
+  describe 'DELETE /api/ticket_desks/:id/reservations/:id/tickets/:id' do
     let!(:ticket) { create(:ticket) }
 
     it 'works and return status 204' do
-      delete("/api/ticket_desks/#{ticket_desk}/tickets/#{ticket.id}")
+      delete("/api/ticket_desks/#{ticket_desk}/reservations/#{reservation}/tickets/#{ticket.id}")
       expect(response.status).to eq(204)
     end
   end
