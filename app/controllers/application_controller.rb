47cff17fa3c3072678fe6eacd15b0f7b8f4e802a
Patryk Gramatowski <patrick.gramatowski@gmail.com>
2021-06-10 14:44:46 +0200

    Update

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,15 +1,6 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::API
-  include ActionController::RequestForgeryProtection
-  ActionDispatch::Response.default_headers = {
-    'X-Frame-Options' => 'SAMEORIGIN',
-    'X-Content-Type-Options' => 'nosniff',
-    'X-XSS-Protection' => '0'
-  }
-
-  protect_from_forgery
-
   # JSON API modules
   include JSONAPI::Fetching
   include JSONAPI::Errors
