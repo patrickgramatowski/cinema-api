15bdd9752570af88d8cc3b53ff267da43dd1f505
Patryk Gramatowski <patrick.gramatowski@gmail.com>
2021-06-10 11:45:20 +0200

    Delete unused comments

--- config/application.rb
+++ config/application.rb
@@ -12,8 +12,6 @@ require "action_mailbox/engine"
 require "action_text/engine"
 require "action_view/railtie"
 require "action_cable/engine"
-# require "sprockets/railtie"
-# require "rails/test_unit/railtie"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -24,18 +22,6 @@ module CinemaApi
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
     config.paths.add 'lib', eager_load: true
-
-    # Configuration for the application, engines, and railties goes here.
-    #
-    # These settings can be overridden in specific environments using the files
-    # in config/environments, which are processed later.
-    #
-    # config.time_zone = "Central Time (US & Canada)"
-    # config.eager_load_paths << Rails.root.join("extras")
-
-    # Only loads a smaller set of middleware suitable for API only apps.
-    # Middleware like session, flash, cookies can be added back manually.
-    # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
     config.active_job.queue_adapter = :delayed_job
   end
