e3c0d8a63977d39ca7df503f9777e8b6e6488b12
Patryk Gramatowski <patrick.gramatowski@gmail.com>
2021-06-14 10:46:55 +0200

    Update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,7 @@
 Rails.application.routes.draw do
   # JSON:API
   namespace :api do
-    resources :halls, :movies, :seances
-    
-    resources :ticket_desks do
-      resources :reservations do 
-        resources :tickets
-      end
-    end
+    resources :halls, :movies, :seances, :reservations, :ticket_desks
+    resources :tickets, only: %i[show index]
   end
 end
