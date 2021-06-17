fb4632c310fb069baa86c397d6895549a2a170ba
Patryk Gramatowski <patrick.gramatowski@gmail.com>
2021-06-17 15:35:45 +0200

    Create/update controllers for user model, registartions & sessions - devise

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,4 +9,12 @@ class ApplicationController < ActionController::API
   include ActionController::MimeResponds
 
   respond_to :jsonapi
+
+  def render_jsonapi_response(resource)
+    if resource.errors.empty?
+      render jsonapi: resource
+    else
+      render jsonapi_errors: resource.errors, status: 400
+    end
+  end
 end
