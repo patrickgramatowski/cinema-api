744f757de727f37db8aae5b5852bb7ff2e53c992
Patryk Gramatowski <patrick.gramatowski@gmail.com>
2021-06-17 15:34:32 +0200

    Add tests for user model / login / registration

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,6 @@
 FactoryBot.define do
   factory :user do
-    
+    sequence(:email) { |n| "example_#{n}@email.com" }
+    password { "testtest" }
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'validations' do
+    subject { build(:user) }
+    context 'has email attribute' do
+      it 'must be present' do
+        expect(subject).to be_valid
+        subject.email = nil
+        expect(subject).to_not be_valid
+      end
+    end
+
+    context 'has password attribute' do
+      it 'must be present' do
+        expect(subject).to be_valid
+        subject.password = nil
+        expect(subject).to_not be_valid
+      end
+    end
+  end
 end
