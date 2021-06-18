89f1171e551f5644e76a860ce232aa004a81961e
Patryk Gramatowski <patrick.gramatowski@gmail.com>
2021-06-18 02:43:44 +0200

    Update use_cases

--- app/concepts/reservations/use_cases/create_online.rb
+++ app/concepts/reservations/use_cases/create_online.rb
@@ -3,17 +3,18 @@
 module Reservations
   module UseCases
     class CreateOnline
-      attr_reader :repository, :params
+      attr_reader :repository, :params, :user
 
-      def initialize(params:, repository: Reservations::Repository.new)
+      def initialize(params:, user:, repository: Reservations::Repository.new)
         @repository = repository
         @params = params
+        @user = user
       end
 
       def call
         ActiveRecord::Base.transaction do
           repository.create!(reservation_params).tap do |reservation|
-            Tickets::UseCases::Create.new(reservation: reservation, tickets: params[:tickets]).call
+            Tickets::UseCases::Create.new(reservation: reservation, tickets: ticket_params(params[:tickets])).call
             cancel_expired(reservation: reservation)
           end
         end
@@ -25,8 +26,21 @@ module Reservations
         {
           seance_id: params[:seance_id],
           ticket_desk_id: TicketDesk.where(online: true).first.id,
-          status: "pending"
+          status: "pending",
+          user_id: user
         }
+      end
+
+      def ticket_params(tickets)
+        tickets_list = []
+        tickets.map do |ticket|
+          tickets_list << {
+            seat: ticket[:seat],
+            ticket_type: ticket[:ticket_type],
+            price: 22 # todo
+          }
+        end
+        tickets_list
       end
 
       def cancel_expired(reservation:)
