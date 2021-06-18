c78611169b9450bb4ace821d6d1b7095f2a7ea8f
Patrick Gramatowski <63557278+patrickgramatowski@users.noreply.github.com>
2021-06-18 20:21:06 +0200

    Add seeds (#28)

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,18 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+# Create ticket desks
+TicketDesk.create(online: true)
+4.times { TicketDesk.create(online: false) }
+
+# Create halls
+Hall.create(hall: 'Hall 1', seats: 200)
+Hall.create(hall: 'Hall 2', seats: 100)
+Hall.create(hall: 'Hall 3', seats: 100)
+Hall.create(hall: 'Hall 4', seats: 100)
+Hall.create(hall: 'Hall 5', seats: 100)
+Hall.create(hall: 'Hall 6', seats: 50)
+Hall.create(hall: 'Hall 7', seats: 50)
+Hall.create(hall: 'Hall 8', seats: 50)
+Hall.create(hall: 'Hall 9', seats: 50)
+Hall.create(hall: 'Hall 10', seats: 20)
+
+# Create 'abstract' user
+User.create(id: 999, email: "abstract_user@gmail.com", password: "wordpass" employee: false)
