93b8d0bc2c9ce91e94e257e0c0ec86cad5c24cd3
Patryk Gramatowski <patrick.gramatowski@gmail.com>
2021-06-18 02:42:45 +0200

    Update reservations tests - add auth concept

--- spec/concepts/reservations/use_cases/create_online_spec.rb
+++ spec/concepts/reservations/use_cases/create_online_spec.rb
@@ -2,9 +2,11 @@ require "rails_helper"
 
 RSpec.describe Reservations::UseCases::CreateOnline do
   describe ".call" do
+    let!(:ticket_desk) { create(:ticket_desk) }
     let(:seance) { create(:seance, id: 78)}
     let(:params) { { seance_id: seance.id, tickets: [{ seat: "5D", price: 21, ticket_type: "normal" }] } }
-    let(:instance) { described_class.new(params: params) }
+    let(:user) { create(:user, id: 11) }
+    let(:instance) { described_class.new(params: params, user: user.id) }
 
     it "creates reservation" do
       expect { instance.call }.to change { Reservation.count }.by(1)
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,5 +2,6 @@ FactoryBot.define do
   factory :user do
     sequence(:email) { |n| "example_#{n}@email.com" }
     password { "testtest" }
+    employee { true }
   end
 end
