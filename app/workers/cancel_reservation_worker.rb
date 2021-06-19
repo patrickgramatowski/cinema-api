202db2fffc34ec8e791d578bd8d02ceefff8b557
Patryk Gramatowski <patrick.gramatowski@gmail.com>
2021-06-19 21:47:44 +0200

    Add CancellReservation method - worker

--- app/workers/cancel_reservation_worker.rb
+++ app/workers/cancel_reservation_worker.rb
@@ -8,5 +8,6 @@ class CancelReservationWorker
     return unless reservation.expired?
 
     reservation.update!(status: "cancelled")
+    ReservationMailer.cancel_reservation(User.find(id: reservation.user_id).email, reservation).deliver_now
   end
 end
