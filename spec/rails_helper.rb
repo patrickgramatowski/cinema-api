55d911cac67be6c403eeaafd3d3bb6d9520c4dac
Patryk Gramatowski <patrick.gramatowski@gmail.com>
2021-06-19 13:28:37 +0200

    Update specs

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -31,6 +31,7 @@ rescue ActiveRecord::PendingMigrationError => e
   exit 1
 end
 RSpec.configure do |config|
+  ActiveRecord::Migration.maintain_test_schema!
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
 
