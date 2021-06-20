1a7034bc57c2e2faab4224a60a60a79d43f3adee
Patryk Gramatowski <patrick.gramatowski@gmail.com>
2021-06-20 18:06:06 +0200

    Refactor halls test

--- spec/requests/halls_spec.rb
+++ spec/requests/halls_spec.rb
@@ -8,35 +8,67 @@ RSpec.describe "Halls requests", type: :request do
   describe "GET /api/halls" do
     let!(:hall) { build(:hall) }
 
-    it "works and return status 200" do
-      get("/api/halls", headers: setup_request(user_2))
-      expect(response.status).to eq(200)
+    context "when user is logged in" do
+      it "works if use is employee" do
+        get("/api/halls", headers: setup_request(user_2))
+        expect(response.status).to eq(200)
+      end
+
+      it "redirects if user is not employee" do
+        get("/api/halls", headers: setup_request(user_1))
+        expect(response.status).to eq(302)
+      end
     end
 
-    it "redirects and return status 302" do
-      get("/api/halls", headers: setup_request(user_1))
-      expect(response.status).to eq(302)
+    context "when user is not logged in" do
+      it "redirects" do
+        get("/api/halls")
+        expect(response.status).to eq(302)
+      end
     end
   end
 
   describe "GET /api/halls/:id" do
     let!(:hall) { build(:hall) }
 
-    it "works and return status 200" do
-      get("/api/halls/#{hall.id}", headers: setup_request(user_2))
-      expect(response.status).to eq(200)
+    context "when user is logged in" do
+      it "works if user is employee" do
+        get("/api/halls/#{hall.id}", headers: setup_request(user_2))
+        expect(response.status).to eq(200)
+      end
+
+      it "redirects if user is not employee" do
+        get("/api/halls/#{hall.id}", headers: setup_request(user_1))
+        expect(response.status).to eq(302)
+      end
+    end
+
+    context "when user is not logged in" do
+      it "redirects" do
+        get("/api/halls/#{hall.id}")
+        expect(response.status).to eq(302)
+      end
     end
   end
 
   describe "POST /api/halls" do
-    it "works and return status 201" do
-      post("/api/halls", headers: setup_request(user_2), params: { hall: {  hall: "Hall_2", seats: 150 } }.to_json )
-      expect(response.status).to eq(201)
+    context "when user is logged in" do
+      it "works if user is employee" do
+        post("/api/halls", headers: setup_request(user_2), params: { hall: {  hall: "Hall_2", seats: 150 } }.to_json )
+        expect(response.status).to eq(201)
+      end
+
+      it "redirects if user is not employee" do
+        post("/api/halls", headers: setup_request(user_1), params: { hall: {  hall: "Hall_2", seats: 150 } }.to_json )
+        expect(response.status).to eq(302)
+      end
     end
 
-    it "redirects and return status 302" do
-      post("/api/halls", headers: setup_request(user_1), params: { hall: {  hall: "Hall_2", seats: 150 } }.to_json )
-      expect(response.status).to eq(302)
+    context "when user is not logged in" do
+      it "redirects if user is not employee" do
+        post("/api/halls", params: { hall: {  hall: "Hall_2", seats: 150 } }.to_json )
+        expect(response.status).to eq(302)
+      end
     end
   end
 
@@ -50,24 +82,46 @@ RSpec.describe "Halls requests", type: :request do
   describe 'PUT /api/halls/:id' do
     let!(:hall) { create(:hall) }
 
-    it 'works and returns status 200' do
-      put("/api/halls/#{hall.id}", headers: setup_request(user_2), params: { hall: { id: hall.id, name: "Hall_updated" } }.to_json)
-      expect(response.status).to eq(200)
+    context "when user is logged in" do
+      it "works if user is employee" do
+        put("/api/halls/#{hall.id}", headers: setup_request(user_2), params: { hall: { id: hall.id, name: "Hall_updated" } }.to_json)
+        expect(response.status).to eq(200)
+      end
+
+      it "redirects if user is not employee" do
+        put("/api/halls/#{hall.id}", headers: setup_request(user_1), params: { hall: { id: hall.id, name: "Hall_updated" } }.to_json)
+        expect(response.status).to eq(302)
+      end
     end
 
-    it "redirects and return status 302" do
-      put("/api/halls/#{hall.id}", headers: setup_request(user_1), params: { hall: { id: hall.id, name: "Hall_updated" } }.to_json)
-      expect(response.status).to eq(302)
+    context "when user is not logged in" do
+      it "redirects" do
+        put("/api/halls/#{hall.id}", params: { hall: { id: hall.id, name: "Hall_updated" } }.to_json)
+        expect(response.status).to eq(302)
+      end
     end
-    
   end
 
   describe 'DELETE /api/halls/:id' do
     let!(:hall) { create(:hall) }
 
-    it 'works and return status 204' do
-      delete("/api/halls/#{hall.id}", headers: setup_request(user_2))
-      expect(response.status).to eq(204)
+    context "when user is logged in" do
+      it 'works if user is employee' do
+        delete("/api/halls/#{hall.id}", headers: setup_request(user_2))
+        expect(response.status).to eq(204)
+      end
+
+      it 'redirects if user is not employee' do
+        delete("/api/halls/#{hall.id}", headers: setup_request(user_1))
+        expect(response.status).to eq(302)
+      end
+    end
+
+    context "when user is not logged in" do
+      it 'redirects' do
+        delete("/api/halls/#{hall.id}")
+        expect(response.status).to eq(302)
+      end
     end
   end
 end
